f2a7a567e77b0a03d1ad2890cbf5dda851a0f602
Mark Hayes <mark@zurb.com>
2012-09-05 17:46:24 -0700

    added in rest of docs pages

---
+++ vendor/assets/javascripts/rainbow/index.js
@@ -0,0 +1,7 @@
+//= require rainbow/rainbow
+//= require rainbow/generic
+//= require rainbow/html
+//= require rainbow/css
+//= require rainbow/javascript
+//= require rainbow/shell
+//= require rainbow/ruby
