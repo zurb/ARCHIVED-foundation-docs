26efbaa4346e385d743143000fa0eda59d060b9f
Mark Hayes <mark@zurb.com>
2012-09-05 18:17:26 -0700

    added image assets and setup pjax for docs pages

--- app/controllers/docs/index_controller.rb
+++ app/controllers/docs/index_controller.rb
@@ -1,56 +1,85 @@
 class Docs::IndexController < Docs::BaseController
   def home
+    if request.xhr?
+      render layout: false
+    end
   end
   
-  def installing
-    
+  def install
+    if request.xhr?
+      render layout: false
+    end
   end
   
   def gem_install
-    
+    if request.xhr?
+      render layout: false
+    end
   end
   
   def grid
-    
+    if request.xhr?
+      render layout: false
+    end
   end
   
   def typography
-    
+    if request.xhr?
+      render layout: false
+    end
   end
   
   def buttons
-    
+    if request.xhr?
+      render layout: false
+    end
   end
   
   def forms
-    
+    if request.xhr?
+      render layout: false
+    end
   end
   
   def navigation
-    
+    if request.xhr?
+      render layout: false
+    end
   end
   
   def tabs
-    
+    if request.xhr?
+      render layout: false
+    end
   end
   
   def elements
-    
+    if request.xhr?
+      render layout: false
+    end
   end
   
   def orbit
-    
+    if request.xhr?
+      render layout: false
+    end
   end
   
   def reveal
-    
+    if request.xhr?
+      render layout: false
+    end
   end
   
   def javascripts
-    
+    if request.xhr?
+      render layout: false
+    end
   end
   
   def support
-    
+    if request.xhr?
+      render layout: false
+    end
   end
 end
